fd36b5e7189603420da58871199fd01e39a5aa17
GuidoInsinger <79085571+GuidoInsinger@users.noreply.github.com>
2025-06-20 23:34:45 +0200

    almost back to start

--- basethingy.py
+++ basethingy.py
@@ -5,6 +5,7 @@ import numpy.typing as npt
 
 from utils import dataclass
 
+
 @dataclass
 class StateVector:
     x: np.floating
@@ -15,6 +16,7 @@ class StateVector:
     al: np.floating
     ar: np.floating
 
+
 @dataclass
 class Measurement:
     a_x: np.floating
@@ -23,26 +25,26 @@ class Measurement:
     x_gps: np.floating
     y_gps: np.floating
 
+
 @dataclass
 class Controls:
     v_l_desired: np.floating
     v_r_desired: np.floating
+
 
 @dataclass
 class Robot:
     L: np.floating
     epsilon: np.floating
 
+
 class EKF:
     def __init__(
         self,
         robot: Robot,
-
         P0: Annotated[npt.NDArray[np.float64], Literal["N", "N"]],
-
         Q: Annotated[npt.NDArray[np.float64], Literal["N", "N"]],
         R: Annotated[npt.NDArray[np.float64], Literal["M", "M"]],
-
     ) -> None:
         self.robot = robot
         self.P = P0
@@ -50,31 +52,62 @@ class EKF:
         self.R = R
 
     def predict(
-        self, state:StateVector, controls: Controls, dt: float
-    ) -> 
-        x_pred = state.x + (state.vl+state.vr)*np.cos(state.theta)/2*dt
-        y_pred = state.y + (state.vl+state.vr)*np.cos(state.theta)/2*dt
-        theta_pred = state.theta + (-state.vl+state.vr)/self.robot.L*dt
-        vl_pred = state.vl + state.al*dt
-        vr_pred = state.vr + state.ar*dt
+        self, state: StateVector, controls: Controls, dt: float
+    ) -> tuple[StateVector, Annotated[npt.NDArray[np.float64], Literal["N", "N"]]]:
+        x_pred = state.x + (state.vl + state.vr) * np.cos(state.theta) / 2 * dt
+        y_pred = state.y + (state.vl + state.vr) * np.cos(state.theta) / 2 * dt
+        theta_pred = state.theta + (-state.vl + state.vr) / self.robot.L * dt
+        vl_pred = state.vl + state.al * dt
+        vr_pred = state.vr + state.ar * dt
 
         delta_vl = controls.v_l_desired - state.vl
         delta_vr = controls.v_r_desired - state.vr
 
-        al_pred = cast(np.floating, 0.5*np.sign(delta_vl) if delta_vl > self.robot.epsilon else 0.0)
-        ar_pred = cast(np.floating, 0.5*np.sign(delta_vr) if delta_vr > self.robot.epsilon else 0.0)
+        al_pred = cast(
+            np.floating,
+            0.5 * np.sign(delta_vl) if delta_vl > self.robot.epsilon else 0.0,
+        )
+        ar_pred = cast(
+            np.floating,
+            0.5 * np.sign(delta_vr) if delta_vr > self.robot.epsilon else 0.0,
+        )
 
-        x_prior = StateVector(x = x_pred, 
-                              y = y_pred, 
-                              theta = 
-                              theta_pred, 
-                              vl=vl_pred,
-                              vr=vr_pred,
-                              al=al_pred,
-                              ar=ar_pred)
-        
-        F = np.array([[1.0, 0.0, (state.vr+state.vl)*np.sin(state.theta)]])
-        cov_prior = F_current @ self.P @ F_current.T + Q_current
+        x_prior = StateVector(
+            x=x_pred,
+            y=y_pred,
+            theta=theta_pred,
+            vl=vl_pred,
+            vr=vr_pred,
+            al=al_pred,
+            ar=ar_pred,
+        )
+
+        F = np.array(
+            [
+                [
+                    1.0,
+                    0.0,
+                    -(state.vl + state.vr) / 2 * np.sin(state.theta),
+                    np.cos(state.theta) * dt / 2,
+                    np.cos(state.theta) * dt / 2,
+                    0.0,
+                    0.0,
+                ],
+                [
+                    0.0,
+                    1.0,
+                    (state.vl + state.vr) / 2 * np.cos(state.theta),
+                    np.sin(state.theta) * dt / 2,
+                    np.sin(state.theta) * dt / 2,
+                    0.0,
+                    0.0,
+                ],
+                [0.0, 0.0, 1.0, -dt / self.robot.L, dt / self.robot.L, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0, 0.0, state.al * dt, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, state.ar * dt],
+            ]
+        )
+        cov_prior = F @ self.P @ F.T + self.Q
 
         return x_prior, cov_prior
 
